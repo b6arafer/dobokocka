961cf9db3c7fd26f072dde30844649bd95d96c94
diak <diak@10.9.8.5>
2019-02-21 08:13:23 +0100

    felig kesz

--- index.php
+++ index.php
@@ -2,7 +2,12 @@
     include("kocka.php");
     $kocka=new kocka(6);
     $kocka->dobas();
-    echo "$kocka->dobott";
-    
+    echo ("$kocka->dobott");
+    for($i=0;$i<$kocka->dobott;$i++)
+    {
+        echo '<img src=potty.jpg/>';
+        echo '</ br>';
+        
+    }
 ?>
 
